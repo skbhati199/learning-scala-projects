fcb6b5c9dc02dbe9540c466e9b5aaf0c9054acb5
Sonu <skbhati199@gmail.com>
2023-03-15 11:29:36 +0530

    do while, while, for loop

--- Scala For Loop/src/Main.scala
+++ Scala For Loop/src/Main.scala
@@ -2,11 +2,32 @@ object Main {
   def main(args: Array[String]): Unit = {
 //    println("Hello world!")
 
-    var a = 0
-    var b= 0
-    for (a <- -1 to 3 ; b <- -1 to 3) {
-      println("Value of a is " + a)
-      println("Value of b is " + b)
+    // for loop nested loop
+//    for (a <- -1 to 3 ; b <- -1 to 3) {
+//      println("Value of a is " + a)
+//      println("Value of b is " + b)
+//    }
+
+    // for loop with conditions
+//    var a = 0
+//    var numList= List(1,2,3,4,6,8,11)
+//    for (a <- numList if a != 3 ; if a < 8){
+//      println("Value a of " + a)
+//    }
+
+    // do-while loop
+//    var a = 10
+//    do {
+//      println("Value of a is " + a)
+//      a = a + 1
+//    } while (a < 20)
+//
+//    println("Final value " + a)
+
+    // while loop
+    var a = 10
+    while (a < 10) {
+      println("Hello ")
     }
   }
 }
